f4ad5576ce291e202d0e21ef4699c3e28082270c
Charlie <charlie@wizeline.com>
2018-02-09 16:23:35 -0600

    using types

--- FloatingScrollView.js
+++ FloatingScrollView.js
@@ -1,11 +1,9 @@
-import React, {Component, PropTypes} from 'react'
-import {
-  StyleSheet,
-  ScrollView,
-  Animated,
-  View,
-  Easing
-} from 'react-native';
+// @flow
+
+import React, { Component } from 'react';
+// eslint ignore next $FlowFixMe
+import { StyleSheet, ScrollView, Animated, View, Easing } from 'react-native';
+import type { Props, ScrollEvent } from './FloatingScrollView.type';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,37 +15,52 @@ const styles = StyleSheet.create({
   },
 });
 
-class FloatingScrollView extends Component {
-  constructor(props) {
+class FloatingScrollView extends Component<Props> {
+  isAnimating: boolean;
+  scrollPoint: number;
+  animatedValue: Animated.Value;
+
+  static defaultProps = {
+    offset: 50,
+    height: 50,
+    bottom: 30,
+  };
+
+  constructor(props: Props) {
     super(props);
     this.isAnimating = false;
     this.scrollPoint = 0;
-    this.onScroll = this.onScroll.bind(this);
   }
 
   componentWillMount() {
-    this._animatedValue = new Animated.Value(this.props.bottom);
+    this.animatedValue = new Animated.Value(this.props.bottom);
   }
 
-  onScroll(event) {
-    console.log(event.nativeEvent)
+  onScroll = (event: ScrollEvent) => {
     const offsetY = event.nativeEvent.contentOffset.y;
-    if (offsetY < 0 || this.isAnimating) return;
-    const offset = this.scrollPoint - offsetY;
-    if (offset < -this.props.offset) this.toggleFab(offsetY, -this.props.height);
-    if (offset > this.props.offset) this.toggleFab(offsetY, this.props.bottom);
-  }
 
-  toggleFab(newPoint, position) {
+    if (offsetY < 0 || this.isAnimating) {
+      return;
+    }
+
+    const offset = this.scrollPoint - offsetY;
+
+    if (offset < -this.props.offset) {
+      this.toggleFab(offsetY, -this.props.height);
+    }
+
+    if (offset > this.props.offset) {
+      this.toggleFab(offsetY, this.props.bottom);
+    }
+  };
+
+  toggleFab(newPoint: number, position: number) {
     this.isAnimating = true;
-    Animated.timing(
-      this._animatedValue,
-      {
-        toValue: position,
-        duration: 200,
-        easing: Easing.in,
-      }
-    ).start(() => {
+    Animated.timing(this.animatedValue, {
+      toValue: position,
+      duration: 200,
+      easing: Easing.in,
+    }).start(() => {
       this.isAnimating = false;
       this.scrollPoint = newPoint;
     });
@@ -57,33 +70,25 @@ class FloatingScrollView extends Component {
     return (
       <View style={styles.container}>
         <ScrollView
-          ref='_scrollView'
+          ref="_scrollView"
           scrollEventThrottle={16}
           onScroll={this.onScroll}
-          style={styles.scrollView}>
+          style={styles.scrollView}
+        >
           {this.props.children}
         </ScrollView>
         <Animated.View
-          style={[styles.animatedView, this.props.childrenStyles,{bottom: this._animatedValue}]}>
+          style={[
+            styles.animatedView,
+            this.props.childrenStyles,
+            { bottom: this.animatedValue },
+          ]}
+        >
           {this.props.floatingView}
         </Animated.View>
       </View>
-    )
+    );
   }
-
 }
-
-FloatingScrollView.propTypes = {
-  floatingView: PropTypes.node.isRequired,
-  offset: PropTypes.number,
-  height: PropTypes.number,
-  bottom: PropTypes.number,
-}
-
-FloatingScrollView.defaultProps = {
-  offset: 50,
-  height: 50,
-  bottom: 30,
-};
 
 export default FloatingScrollView;
---
+++ FloatingScrollView.type.js
@@ -0,0 +1,30 @@
+// @flow
+
+import type { Node } from 'react';
+
+export type Props = {
+  bottom: number,
+  children: Node,
+  childrenStyles: Object,
+  floatingView: Node,
+  height: number,
+  offset: number,
+};
+
+export type ScrollEvent = {
+  nativeEvent: {
+    contentOffset: {
+      x: number,
+      y: number,
+    },
+    contentSize: {
+      height: number,
+      width: number,
+    },
+    layoutMeasurement: {
+      height: number,
+      width: number,
+    },
+  },
+  timeStamp: number,
+};
